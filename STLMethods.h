9b48402bc201adc75e4eb6f13938382f9ba63958
senconscious <43296143+senconscious@users.noreply.github.com>
2019-12-24 15:25:41 +0300

    Add files via upload

--- STLMethods.h
+++ STLMethods.h
@@ -7,14 +7,17 @@ using namespace std;
 #include <vector>
 
 
-
+//Swaps first and Last elements in the string and returns it. String must be of event length
 string swapFirstLast(string o);
 
+//Swaps middle elements in the string and returns it. String must be of even length
 string swapMiddleElements(string o);
 
+//Inserts Five Default Value Elements in middle of the Vector. Vector must be of even length
 template<typename T>
 void insertFiveElementsMiddle(vector<T>& o);
 
+//Lambda Function to cout elements
 template<typename T>
 auto print = [](const T& o) {cout << " " << o; };
 
